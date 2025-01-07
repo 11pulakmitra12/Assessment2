40065dfae82acf9d03a8e28a3c38d859bcd05014
11pulakmitra12 <11pulakmitra12@gmail.com>
2025-01-07 21:39:11 +0530

    added multiple condition and test cases

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 
-function add() {
-    return 0;
+// function add() {
+//     return 0;
+// }
+
+
+// function add(num1, num2) {
+//     if(num1 && num2){
+//         return num1+ num2;
+//     }
+//     else return 0;
+// }
+
+
+function add(...numbers) {
+    if (numbers.length > 0) {
+        return numbers.reduce((sum, num) => sum + num, 0);
+    }
+    else return 0;
 }
 
 module.exports = {
--- test/adder.spec.js
+++ test/adder.spec.js
@@ -6,5 +6,15 @@ describe("adder", () => {
     it("should return 0 for no inputs", () => {
         const result = add();
         assert.equal(result, 0);
-    })
+    });
+
+    it("should return addition of 2 numbers",()=>{
+        const result =add(3,5);
+        assert.equal(result,8);
+    });
+
+    it("should return addition of n numbers",()=>{
+        const result =add(1,2,3,4,5);
+        assert.equal(result,15);
+    });
 })
